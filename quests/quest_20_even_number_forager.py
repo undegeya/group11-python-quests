2fc2415d5bfcc18d258c714de073674bf88e36e8
NDEGEYA Nina UWASE <u.ndegeya@alustudent.com>
2026-06-29 21:14:32 +0200

    Commiting my work on quest 19 to quest 24 as member 4

---
+++ quests/quest_20_even_number_forager.py
@@ -0,0 +1,5 @@
+# Quest 20: Printing The Even Numbers
+
+for number in range(1, 21):
+    if number % 2 == 0:
+        print(number)
---
+++ quests/quest_21_reusable_incantation.py
@@ -0,0 +1,9 @@
+# Quest 21: The Reusable Incantation(calling function)
+
+def greet_adventurer():
+    print("Welcome, brave adventurer!")
+
+
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
